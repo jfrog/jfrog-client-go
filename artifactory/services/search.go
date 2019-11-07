d80e70f3b0129bdd63a9f3487d5da3e33b9d04c3
Yahav Itzhak <yahavi@users.noreply.github.com>
2019-11-07 12:06:03 +0200

    Optimize search by build without pattern (#95)

--- artifactory/services/search.go
+++ artifactory/services/search.go
@@ -55,6 +55,8 @@ func SearchBySpecFiles(searchParams SearchParams, flags utils.CommonConf, requir
 	switch searchParams.GetSpecType() {
 	case utils.WILDCARD:
 		itemsFound, err = utils.SearchBySpecWithPattern(searchParams.GetFile(), flags, requiredArtifactProps)
+	case utils.BUILD:
+		itemsFound, err = utils.SearchBySpecWithBuild(searchParams.GetFile(), flags)
 	case utils.AQL:
 		itemsFound, err = utils.SearchBySpecWithAql(searchParams.GetFile(), flags, requiredArtifactProps)
 	}
