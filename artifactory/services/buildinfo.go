ad1dbadfc174ce5d7fc95b8ff6a7558278d0b841
Or Gabay <orgab@jfrog.com>
2020-01-13 15:24:03 +0200

    Buildinfo - accept httpcode 200

--- artifactory/services/buildinfo.go
+++ artifactory/services/buildinfo.go
@@ -92,7 +92,7 @@ func (bis *BuildInfoService) PublishBuildInfo(build *buildinfo.BuildInfo) error 
 	if err != nil {
 		return err
 	}
-	if resp.StatusCode != http.StatusCreated && resp.StatusCode != http.StatusNoContent {
+	if resp.StatusCode != http.StatusCreated && resp.StatusCode != http.StatusNoContent && resp.StatusCode != http.StatusOK {
 		return errorutils.CheckError(errors.New("Artifactory response: " + resp.Status + "\n" + clientutils.IndentJson(body)))
 	}
 
