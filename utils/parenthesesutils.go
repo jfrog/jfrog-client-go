78e3725b364b449b6147dd65290b308ad6381f55
barbelity <barb@jfrog.com>
2020-06-08 21:23:39 +0300

    Support downloading files which use non-ASCII characters in their name

--- utils/parenthesesutils.go
+++ utils/parenthesesutils.go
@@ -35,13 +35,12 @@ func RemovePlaceholderParentheses(pattern, target string) string {
 	parentheses := NewParenthesesSlice(pattern, target)
 	// Remove parentheses which have a corresponding placeholder.
 	var temp string
-	for i := 0; i < len(pattern); i++ {
-		if (pattern[i] == '(' || pattern[i] == ')') && parentheses.IsPresent(i) {
+	for i, c := range pattern {
+		if (c == '(' || c == ')') && parentheses.IsPresent(i) {
 			continue
 		} else {
-			temp = temp + string(pattern[i])
+			temp = temp + string(c)
 		}
-
 	}
 	return temp
 }
@@ -50,11 +49,11 @@ func RemovePlaceholderParentheses(pattern, target string) string {
 func addEscapingParentheses(pattern, target string) string {
 	parentheses := NewParenthesesSlice(pattern, target)
 	var temp string
-	for i := 0; i < len(pattern); i++ {
-		if (pattern[i] == '(' || pattern[i] == ')') && !parentheses.IsPresent(i) {
-			temp = temp + "\\" + string(pattern[i])
+	for i, c := range pattern {
+		if (c == '(' || c == ')') && !parentheses.IsPresent(i) {
+			temp = temp + "\\" + string(c)
 		} else {
-			temp = temp + string(pattern[i])
+			temp = temp + string(c)
 		}
 	}
 	return temp
