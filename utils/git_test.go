3caaf4912407db5e48d06660af1a72efb3673108
robin <robin@jfrog.com>
2020-06-16 18:21:28 +0300

    Add test, remove redundant files

--- utils/git_test.go
+++ utils/git_test.go
@@ -1,22 +1,22 @@
 package utils
 
 import (
+	"github.com/stretchr/testify/assert"
+	"path/filepath"
 	"testing"
 )
 
 func TestGitManager(t *testing.T) {
-	projectPath := initVcsTestDir(t)
-	gitManager := NewGitManager(projectPath)
-	err := gitManager.ReadConfig()
-	if err != nil {
-		t.Error(err)
-	}
-	want := "https://github.com/jfrog/jfrog-cli.git"
-	if gitManager.GetUrl() != want {
-		t.Errorf("TestGitManager() error, want %s, got %s", want, gitManager.GetUrl())
-	}
-	want = "d63c5957ad6819f4c02a817abe757f210d35ff92"
-	if gitManager.GetRevision() != want {
-		t.Errorf("TestGitManager() error, want %s, got %s", want, gitManager.GetRevision())
+	// Test the following .git types, on their corresponding paths in testsdata.
+	tests := []string{"vcs", "packedVcs"}
+	for _, test := range tests {
+		t.Run(test, func(t *testing.T) {
+			projectPath := initVcsTestDir(t, filepath.Join("testsdata", test))
+			gitManager := NewGitManager(projectPath)
+			err := gitManager.ReadConfig()
+			assert.NoError(t, err)
+			assert.Equal(t, "https://github.com/jfrog/jfrog-cli.git", gitManager.GetUrl())
+			assert.Equal(t, "d63c5957ad6819f4c02a817abe757f210d35ff92", gitManager.GetRevision())
+		})
 	}
 }
