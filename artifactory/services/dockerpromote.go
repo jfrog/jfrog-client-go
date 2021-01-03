8246d737371a8cf28ab618a1ee390887fbd977a0
Yahav Itzhak <yahavi@users.noreply.github.com>
2021-01-03 08:44:49 +0200

    Add success message to docker-promote (#268)

--- artifactory/services/dockerpromote.go
+++ artifactory/services/dockerpromote.go
@@ -73,8 +73,9 @@ func (ps *DockerPromoteService) PromoteDocker(params DockerPromoteParams) error 
 	if resp.StatusCode != http.StatusOK {
 		return errorutils.CheckError(errors.New("Artifactory response: " + resp.Status + "\n" + clientutils.IndentJson(body)))
 	}
+	
 	log.Debug("Artifactory response: ", resp.Status)
-
+	log.Info("Promoted image", params.SourceDockerImage, "to:", params.TargetRepo, "repository.")
 	return nil
 }
 
