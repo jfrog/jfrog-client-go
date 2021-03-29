214389cc6a8ed2f45df4fe890dc153548dee412a
Sebin John <sebinjohn@users.noreply.github.com>
2021-03-30 01:01:48 +1100

    Add test: getUserTokensTest

--- tests/artifactorysecurity_test.go
+++ tests/artifactorysecurity_test.go
@@ -136,6 +136,42 @@ func getTokensTest(t *testing.T) {
 	tokensToRevoke = append(tokensToRevoke, token.RefreshToken)
 }
 
+func getUserTokenstest(t *testing.T) {
+	token, err := createToken()
+	if err != nil {
+		t.Error(err)
+	}
+	tokens, err := testsSecurityService.GetUserTokens("anonymous")
+	if len(tokens) != 1 {
+		t.Error("Failed to get tokens of anonymous user")
+	}
+	if tokens[0] != token.AccessToken {
+		t.Error("Retried user token doesn't match expected token value")
+	}
+	params := services.NewCreateTokenParams()
+	params.Username = "test-user"
+	params.Scope = "api:* member-of-groups:readers"
+	params.Refreshable = true  // We need to use the refresh token to revoke these tokens on teardown
+	params.Audience = "jfrt@*" // Allow token to be accepted by all instances of Artifactory.
+
+	token1, err := testsSecurityService.CreateToken(params)
+	if err != nil {
+		t.Error(err)
+	}
+
+	token2, err := testsSecurityService.CreateToken(params)
+	if err != nil {
+		t.Error(err)
+	}
+	tokens, err = testsSecurityService.GetUserTokens("test-user")
+	if len(tokens) != 2 {
+		t.Error("Failed to get tokens of test-user")
+	}
+	if tokens[0] != token1.AccessToken || tokens[1] != token2.AccessToken {
+		t.Error("Retried user token doesn't match expected token value")
+	}
+}
+
 // Util function to revoke a token
 func revokeToken(token string) (string, error) {
 	params := services.NewRevokeTokenParams()
