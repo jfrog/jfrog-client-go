0f30782806d5cffa981c8d21b97c91fe3e50902e
sverdlov93 <sverdlov93@gmail.com>
2021-08-04 17:53:10 +0300

    Add support for commands with build and project flags combined, and also api to artifactory Access/projects RestApi.
    Signed-off-by: sverdlov93 <sverdlov93@gmail.com>

--- tests/accessproject_test.go
+++ tests/accessproject_test.go
@@ -9,35 +9,10 @@ import (
 
 func TestAccessProject(t *testing.T) {
 	initAccessTest(t)
-	t.Run("create", testAccessCreateProject)
-	t.Run("update", testAccessUpdateProject)
-	t.Run("delete", testAccessDeleteProject)
+	t.Run("create-update-delete", testAccessProjectCreateUpdateDelete)
 }
 
-func testAccessCreateProject(t *testing.T) {
-	projectParams := getTestProjectParams()
-	err := testsAccessProjectService.CreateProject(projectParams)
-	defer deleteProjectAndAssert(t, projectParams.ProjectDetails.ProjectKey)
-	assert.NoError(t, err)
-	createdProject, err := testsAccessProjectService.GetProject(projectParams.ProjectDetails.ProjectKey)
-	assert.NotNil(t, createdProject)
-	if !reflect.DeepEqual(projectParams.ProjectDetails, *createdProject) {
-		t.Error("Unexpected project details built. Expected: `", projectParams.ProjectDetails, "` Got `", *createdProject, "`")
-	}
-}
-
-func testAccessDeleteProject(t *testing.T) {
-	projectParams := getTestProjectParams()
-	err := testsAccessProjectService.CreateProject(projectParams)
-	assert.NoError(t, err)
-	err = testsAccessProjectService.DeleteProject(projectParams.ProjectDetails.ProjectKey)
-	assert.NoError(t, err)
-	updatedProject, err := testsAccessProjectService.GetProject(projectParams.ProjectDetails.ProjectKey)
-	assert.NoError(t, err)
-	assert.Nil(t, updatedProject)
-}
-
-func testAccessUpdateProject(t *testing.T) {
+func testAccessProjectCreateUpdateDelete(t *testing.T) {
 	projectParams := getTestProjectParams()
 	err := testsAccessProjectService.CreateProject(projectParams)
 	defer deleteProjectAndAssert(t, projectParams.ProjectDetails.ProjectKey)
