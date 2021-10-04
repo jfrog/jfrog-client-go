a5439e6d8ba97942479650d8760c5396aac5bb7a
sverdlov93 <sverdlov93@gmail.com>
2021-10-04 14:12:05 +0300

    remove access version test
    Signed-off-by: sverdlov93 <sverdlov93@gmail.com>

--- tests/access_test.go
+++ tests/access_test.go
@@ -1,19 +1,8 @@
 package tests
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
-
-func TestAccessVersion(t *testing.T) {
-	initAccessTest(t)
-	version, err := GetAccessDetails().GetVersion()
-	if err != nil {
-		assert.NoError(t, err)
-		return
-	}
-	assert.NotEmpty(t, version)
-}
 
 func initAccessTest(t *testing.T) {
 	if !*TestAccess {
