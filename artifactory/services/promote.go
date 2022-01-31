54e4992d7ce877c59b60ffa5162a07f473f1110e
sverdlov93 <sverdlov93@gmail.com>
2022-01-31 18:09:26 +0200

    build promotion failFast=false error logs
    Signed-off-by: sverdlov93 <sverdlov93@gmail.com>

--- artifactory/services/promote.go
+++ artifactory/services/promote.go
@@ -93,6 +93,7 @@ type BuildPromotionBody struct {
 	IncludeDependencies *bool  `json:"dependencies,omitempty"`
 	Copy                *bool  `json:"copy,omitempty"`
 	// FailFast options default is true. We need to avoid omitempty, otherwise, it would be forced to false if omitted.
+	// Notice that when running 'JFrog CLI V1' FailFast's default is false.
 	FailFast   bool                `json:"failFast"`
 	DryRun     *bool               `json:"dryRun,omitempty"`
 	Properties map[string][]string `json:"properties,omitempty"`
