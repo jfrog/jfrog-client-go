2929c6d03263e3cb291f232ba2eeeb84bddaf7c6
Yahav Itzhak <yahavi@users.noreply.github.com>
2022-03-01 09:29:05 +0200

    Support IncludePathPrefixPattern param in replication (#532)

--- tests/artifactoryreplication_test.go
+++ tests/artifactoryreplication_test.go
@@ -14,7 +14,7 @@ func TestReplication(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	err = getPushReplication(t, GetReplicationConfig())
+	err = getAndAssertReplication(t, GetReplicationConfig())
 	if err != nil {
 		t.Error(err)
 	}
@@ -22,7 +22,7 @@ func TestReplication(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	err = getPushReplication(t, nil)
+	err = getAndAssertReplication(t, nil)
 	assert.Error(t, err)
 }
 
@@ -36,10 +36,11 @@ func createReplication() error {
 	params.RepoKey = getRtTargetRepoKey()
 	params.Enabled = true
 	params.SocketTimeoutMillis = 100
+	params.IncludePathPrefixPattern = "/include/path"
 	return testsCreateReplicationService.CreateReplication(params)
 }
 
-func getPushReplication(t *testing.T, expected []utils.ReplicationParams) error {
+func getAndAssertReplication(t *testing.T, expected []utils.ReplicationParams) error {
 	replicationConf, err := testsReplicationGetService.GetReplication(getRtTargetRepoKey())
 	if err != nil {
 		return err
@@ -68,18 +69,18 @@ func deleteReplication(t *testing.T) error {
 func GetReplicationConfig() []utils.ReplicationParams {
 	return []utils.ReplicationParams{
 		{
-			Url:                    "http://www.jfrog.com",
-			Username:               "anonymous",
-			Password:               "password",
-			CronExp:                "0 0 14 * * ?",
-			RepoKey:                getRtTargetRepoKey(),
-			EnableEventReplication: false,
-			SocketTimeoutMillis:    100,
-			Enabled:                true,
-			SyncDeletes:            false,
-			SyncProperties:         false,
-			SyncStatistics:         false,
-			PathPrefix:             "",
+			Url:                      "http://www.jfrog.com",
+			Username:                 "anonymous",
+			Password:                 "password",
+			CronExp:                  "0 0 14 * * ?",
+			RepoKey:                  getRtTargetRepoKey(),
+			EnableEventReplication:   false,
+			SocketTimeoutMillis:      100,
+			Enabled:                  true,
+			SyncDeletes:              false,
+			SyncProperties:           false,
+			SyncStatistics:           false,
+			IncludePathPrefixPattern: "/include/path",
 		},
 	}
 }
