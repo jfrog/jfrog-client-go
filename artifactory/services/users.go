e51443f20c204a262e8d287d75fc22a77e1b4b4a
Tal Arian <tala@jfrog.com>
2022-04-06 19:13:56 +0300

    Add ClearGroups option

--- artifactory/services/users.go
+++ artifactory/services/users.go
@@ -15,6 +15,7 @@ import (
 type UserParams struct {
 	UserDetails     User
 	ReplaceIfExists bool
+	ClearGroups     bool
 }
 
 func NewUserParams() UserParams {
@@ -23,16 +24,16 @@ func NewUserParams() UserParams {
 
 // application/vnd.org.jfrog.artifactory.security.User+json
 type User struct {
-	Name                     string   `json:"name,omitempty" csv:"username,omitempty"`
-	Email                    string   `json:"email,omitempty" csv:"email,omitempty"`
-	Password                 string   `json:"password,omitempty" csv:"password,omitempty"`
-	Admin                    *bool    `json:"admin,omitempty" csv:"admin,omitempty"`
-	ProfileUpdatable         *bool    `json:"profileUpdatable,omitempty" csv:"profileUpdatable,omitempty"`
-	DisableUIAccess          *bool    `json:"disableUIAccess,omitempty" csv:"disableUIAccess,omitempty"`
-	InternalPasswordDisabled *bool    `json:"internalPasswordDisabled,omitempty" csv:"internalPasswordDisabled,omitempty"`
-	LastLoggedIn             string   `json:"lastLoggedIn,omitempty" csv:"lastLoggedIn,omitempty"`
-	Realm                    string   `json:"realm,omitempty" csv:"realm,omitempty"`
-	Groups                   []string `json:"groups,omitempty" csv:"groups,omitempty"`
+	Name                     string    `json:"name,omitempty" csv:"username,omitempty"`
+	Email                    string    `json:"email,omitempty" csv:"email,omitempty"`
+	Password                 string    `json:"password,omitempty" csv:"password,omitempty"`
+	Admin                    *bool     `json:"admin,omitempty" csv:"admin,omitempty"`
+	ProfileUpdatable         *bool     `json:"profileUpdatable,omitempty" csv:"profileUpdatable,omitempty"`
+	DisableUIAccess          *bool     `json:"disableUIAccess,omitempty" csv:"disableUIAccess,omitempty"`
+	InternalPasswordDisabled *bool     `json:"internalPasswordDisabled,omitempty" csv:"internalPasswordDisabled,omitempty"`
+	LastLoggedIn             string    `json:"lastLoggedIn,omitempty" csv:"lastLoggedIn,omitempty"`
+	Realm                    string    `json:"realm,omitempty" csv:"realm,omitempty"`
+	Groups                   *[]string `json:"groups,omitempty" csv:"groups,omitempty"`
 }
 
 type UserService struct {
@@ -112,6 +113,9 @@ func (us *UserService) CreateUser(params UserParams) error {
 }
 
 func (us *UserService) UpdateUser(params UserParams) error {
+	if params.ClearGroups {
+		params.UserDetails.Groups = &[]string{}
+	}
 	url, content, httpDetails, err := us.createOrUpdateUserRequest(params.UserDetails)
 	if err != nil {
 		return err
