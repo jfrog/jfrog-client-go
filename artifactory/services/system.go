915a79ff4ea938519754cc2b8a9f374499c19da9
Yahav Itzhak <yahavi@users.noreply.github.com>
2022-06-06 08:44:54 +0300

    New methods in the system service (#570)

--- artifactory/services/system.go
+++ artifactory/services/system.go
@@ -9,7 +9,10 @@ import (
 	"github.com/jfrog/jfrog-client-go/http/jfroghttpclient"
 	clientutils "github.com/jfrog/jfrog-client-go/utils"
 	"github.com/jfrog/jfrog-client-go/utils/errorutils"
+	"github.com/jfrog/jfrog-client-go/utils/log"
 )
+
+const apiSystem = "api/system/"
 
 type SystemService struct {
 	client     *jfroghttpclient.JfrogHttpClient
@@ -33,14 +36,9 @@ func (ss *SystemService) IsDryRun() bool {
 }
 
 func (ss *SystemService) GetVersion() (string, error) {
-	httpDetails := (*ss.artDetails).CreateHttpClientDetails()
-	resp, body, _, err := ss.client.SendGet((*ss.artDetails).GetUrl()+"api/system/version", true, &httpDetails)
+	body, err := ss.sendGet("version")
 	if err != nil {
 		return "", err
-	}
-
-	if err = errorutils.CheckResponseStatus(resp, http.StatusOK); err != nil {
-		return "", errorutils.CheckError(errorutils.GenerateResponseError(resp.Status, clientutils.IndentJson(body)))
 	}
 	var version artifactoryVersion
 	err = json.Unmarshal(body, &version)
@@ -51,16 +49,64 @@ func (ss *SystemService) GetVersion() (string, error) {
 }
 
 func (ss *SystemService) GetServiceId() (string, error) {
-	httpDetails := (*ss.artDetails).CreateHttpClientDetails()
-	resp, body, _, err := ss.client.SendGet((*ss.artDetails).GetUrl()+"api/system/service_id", true, &httpDetails)
+	body, err := ss.sendGet("service_id")
 	if err != nil {
 		return "", err
 	}
-	if err = errorutils.CheckResponseStatus(resp, http.StatusOK); err != nil {
-		return "", errorutils.CheckError(errorutils.GenerateResponseError(resp.Status, clientutils.IndentJson(body)))
-	}
-
 	return string(body), nil
+}
+
+func (ss *SystemService) GetConfigDescriptor() (string, error) {
+	log.Info("Fetching config descriptor from Artifactory...")
+	body, err := ss.sendGet("configuration")
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (ss *SystemService) ActivateKeyEncryption() error {
+	log.Info("Activating key encryption in Artifactory...")
+	if err := ss.sendEmptyPost("encrypt"); err != nil {
+		return err
+	}
+	log.Info("Artifactory key encryption activated")
+	return nil
+}
+
+func (ss *SystemService) DeactivateKeyEncryption() error {
+	log.Info("Deactivating key encryption in Artifactory...")
+	if err := ss.sendEmptyPost("decrypt"); err != nil {
+		return err
+	}
+	log.Info("Artifactory key encryption deactivated")
+	return nil
+}
+
+func (ss *SystemService) sendGet(endpoint string) ([]byte, error) {
+	httpDetails := (*ss.artDetails).CreateHttpClientDetails()
+	resp, body, _, err := ss.client.SendGet((*ss.artDetails).GetUrl()+apiSystem+endpoint, true, &httpDetails)
+	if err != nil {
+		return nil, err
+	}
+	if err = errorutils.CheckResponseStatus(resp, http.StatusOK, http.StatusCreated); err != nil {
+		return nil, errorutils.CheckError(errorutils.GenerateResponseError(resp.Status, clientutils.IndentJson(body)))
+	}
+	log.Debug("Artifactory response:", resp.Status)
+	return body, nil
+}
+
+func (ss *SystemService) sendEmptyPost(endpoint string) error {
+	httpDetails := (*ss.artDetails).CreateHttpClientDetails()
+	resp, body, err := ss.client.SendPost((*ss.artDetails).GetUrl()+apiSystem+endpoint, nil, &httpDetails)
+	if err != nil {
+		return err
+	}
+	if err = errorutils.CheckResponseStatus(resp, http.StatusOK, http.StatusCreated); err != nil {
+		return errorutils.CheckError(errorutils.GenerateResponseError(resp.Status, clientutils.IndentJson(body)))
+	}
+	log.Debug("Artifactory response:", string(body), resp.Status)
+	return nil
 }
 
 type artifactoryVersion struct {
