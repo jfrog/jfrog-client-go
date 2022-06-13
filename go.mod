12c4bf1fc7d38b28ecde2b7959c97cac3f420176
sverdlov93 <sverdlov93@gmail.com>
2022-06-13 20:36:26 +0300

    remove emojis from terminals that don't support emojis
    Signed-off-by: sverdlov93 <sverdlov93@gmail.com>

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/buger/jsonparser v1.1.1
+	github.com/forPelevin/gomoji v1.1.4
 	github.com/go-git/go-git/v5 v5.4.2
 	github.com/golang-jwt/jwt/v4 v4.4.1
 	github.com/gookit/color v1.5.0
@@ -22,31 +23,38 @@ require (
 	github.com/Microsoft/go-winio v0.5.0 // indirect
 	github.com/ProtonMail/go-crypto v0.0.0-20210428141323-04723f9f07d7 // indirect
 	github.com/acomagu/bufpipe v1.0.3 // indirect
-	github.com/andybalholm/brotli v1.0.1 // indirect
+	github.com/andybalholm/brotli v1.0.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dsnet/compress v0.0.2-0.20210315054119-f66993602bf5 // indirect
 	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/go-git/gcfg v1.5.0 // indirect
 	github.com/go-git/go-billy/v5 v5.3.1 // indirect
-	github.com/golang/snappy v0.0.2 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kevinburke/ssh_config v0.0.0-20201106050909-4977a11b4351 // indirect
-	github.com/klauspost/compress v1.11.4 // indirect
+	github.com/klauspost/compress v1.13.5 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.6 // indirect
 	github.com/klauspost/pgzip v1.2.5 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/matryer/is v1.4.0 // indirect
 	github.com/minio/sha256-simd v1.0.1-0.20210617151322-99e45fae3395 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/nwaples/rardecode v1.1.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.1 // indirect
 	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/ulikunitz/xz v0.5.9 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/sys v0.0.0-20220111092808-5a964db01320 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0 // indirect
 )
 
--- utils/log/logger.go
+++ utils/log/logger.go
@@ -2,12 +2,13 @@ package log
 
 import (
 	"fmt"
+	"github.com/forPelevin/gomoji"
+	"github.com/gookit/color"
+	ioutils "github.com/jfrog/jfrog-client-go/utils/io"
+	"golang.org/x/term"
 	"io"
 	"log"
 	"os"
-
-	"github.com/gookit/color"
-	"golang.org/x/term"
 )
 
 var Logger Log
@@ -21,6 +22,10 @@ var Format LogFormat
 // Determines whether the terminal is available. This variable should not be accessed directly,
 // but through the 'isTerminalMode' function.
 var terminalMode *bool
+
+// Determines whether emoji is supported. This variable should not be accessed directly,
+// but through the 'isEmojiSupported' function.
+var emojiSupported *bool
 
 // defaultLogger is the default logger instance in case the user does not set one
 var defaultLogger = NewLogger(INFO, nil)
@@ -80,24 +85,60 @@ func (logger *jfrogLogger) SetOutputWriter(writer io.Writer) {
 	logger.OutputLog = log.New(writer, "", 0)
 }
 
+func (logger *jfrogLogger) Println(log *log.Logger, values ...interface{}) {
+	if !isEmojiSupported() {
+		for _, value := range values {
+			switch value.(type) {
+			case string:
+				value = gomoji.RemoveEmojis(value.(string))
+			}
+		}
+	}
+	log.Println(values...)
+}
+
 // Set the logs' writer to Stderr unless an alternative one is provided.
 // In case the writer is set for file, colors will not be in use.
 // Log flags to modify the log prefix as described in https://pkg.go.dev/log#pkg-constants.
 func (logger *jfrogLogger) SetLogsWriter(writer io.Writer, logFlags int) {
+	writerIsStdErr := false
 	if writer == nil {
 		writer = os.Stderr
-		if isTerminalMode() {
-			logger.DebugLog = log.New(writer, fmt.Sprintf("[%s] ", color.Cyan.Render("Debug")), logFlags)
-			logger.InfoLog = log.New(writer, fmt.Sprintf("[🔵%s] ", color.Blue.Render("Info")), logFlags)
-			logger.WarnLog = log.New(writer, fmt.Sprintf("[🟠%s] ", color.Yellow.Render("Warn")), logFlags)
-			logger.ErrorLog = log.New(writer, fmt.Sprintf("[🚨%s] ", color.Red.Render("Error")), logFlags)
-			return
-		}
+		writerIsStdErr = true
 	}
-	logger.DebugLog = log.New(writer, "[Debug] ", logFlags)
-	logger.InfoLog = log.New(writer, "[Info] ", logFlags)
-	logger.WarnLog = log.New(writer, "[Warn] ", logFlags)
-	logger.ErrorLog = log.New(writer, "[Error] ", logFlags)
+	logger.DebugLog = log.New(writer, getLogPrefix(DEBUG, writerIsStdErr), logFlags)
+	logger.InfoLog = log.New(writer, getLogPrefix(INFO, writerIsStdErr), logFlags)
+	logger.WarnLog = log.New(writer, getLogPrefix(WARN, writerIsStdErr), logFlags)
+	logger.ErrorLog = log.New(writer, getLogPrefix(ERROR, writerIsStdErr), logFlags)
+}
+
+type prefixStyle struct {
+	logLevel string
+	color    color.Color
+	emoji    string
+}
+
+var prefixStyles = map[LevelType]*prefixStyle{
+	DEBUG: {logLevel: "Debug", color: color.Cyan},
+	INFO:  {logLevel: "Info", emoji: "🔵", color: color.Blue},
+	WARN:  {logLevel: "Warn", emoji: "🟠", color: color.Yellow},
+	ERROR: {logLevel: "Error", emoji: "🚨", color: color.Red},
+}
+
+func getLogPrefix(logType LevelType, writerIsStdErr bool) string {
+	if logPrefixStyle, ok := prefixStyles[logType]; ok {
+		prefix := logPrefixStyle.logLevel
+		// Use colors only on stdErr terminal output
+		if writerIsStdErr && isTerminalMode() {
+			prefix = logPrefixStyle.color.Render(prefix)
+		}
+		if isEmojiSupported() {
+			prefix = logPrefixStyle.emoji + prefix
+		}
+		return fmt.Sprintf("[%s] ", prefix)
+	}
+
+	return ""
 }
 
 func Debug(a ...interface{}) {
@@ -126,30 +167,30 @@ func (logger jfrogLogger) GetLogLevel() LevelType {
 
 func (logger jfrogLogger) Debug(a ...interface{}) {
 	if logger.GetLogLevel() >= DEBUG {
-		logger.DebugLog.Println(a...)
+		logger.Println(logger.DebugLog, a...)
 	}
 }
 
 func (logger jfrogLogger) Info(a ...interface{}) {
 	if logger.GetLogLevel() >= INFO {
-		logger.InfoLog.Println(a...)
+		logger.Println(logger.InfoLog, a...)
 	}
 }
 
 func (logger jfrogLogger) Warn(a ...interface{}) {
 	if logger.GetLogLevel() >= WARN {
-		logger.WarnLog.Println(a...)
+		logger.Println(logger.WarnLog, a...)
 	}
 }
 
 func (logger jfrogLogger) Error(a ...interface{}) {
 	if logger.GetLogLevel() >= ERROR {
-		logger.ErrorLog.Println(a...)
+		logger.Println(logger.ErrorLog, a...)
 	}
 }
 
 func (logger jfrogLogger) Output(a ...interface{}) {
-	logger.OutputLog.Println(a...)
+	logger.Println(logger.OutputLog, a...)
 }
 
 type Log interface {
@@ -167,6 +208,15 @@ func isTerminalMode() bool {
 		terminalMode = &t
 	}
 	return *terminalMode
+}
+
+// Check if Emoji is supported
+func isEmojiSupported() bool {
+	if emojiSupported == nil {
+		t := isTerminalMode() && !ioutils.IsWindows()
+		emojiSupported = &t
+	}
+	return *emojiSupported
 }
 
 // Predefined color formatting functions
