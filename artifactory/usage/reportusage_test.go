cf793de5d034d01d2fe128f4c48c22eb203a2c94
omerzi <omer3914@gmail.com>
2022-09-11 13:04:14 +0300

    Fix reportUsageToJson test

--- artifactory/usage/reportusage_test.go
+++ artifactory/usage/reportusage_test.go
@@ -3,6 +3,7 @@ package usage
 import (
 	"fmt"
 	"github.com/jfrog/jfrog-client-go/utils"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -35,34 +36,41 @@ func TestReportUsageJson(t *testing.T) {
 	type test struct {
 		productId      string
 		commandName    string
+		serviceId      string
+		serverSize     string
 		expectedResult string
+		jsonPatternNum int
 	}
 
-	json := `{"productId":"%s","features":[{"featureId":"%s"}]}`
+	jsonPatterns := []string{
+		`{"productId":"%s","features":[{"featureId":"%s","attributes":{"serverSize":"%s","serviceId":"%s"}}]}`,
+		`{"productId":"%s","features":[{"featureId":"%s"}]}`,
+	}
+
 	preTests := []test{
-		{"jfrog-cli-go/1.26.0", "rt_copy", ""},
-		{"jfrog-client-go", "rt_download", ""},
-		{"test", "rt_build", ""},
-		{"agent/1.25.0", "rt_go", ""},
+		{"jfrog-cli-go/1.26.0", "rt_transfer_files", "jfrt@01g8dj3wcw22y01atqp63n1haq", "6.08 GB", "{\"productId\":\"jfrog-cli-go/1.26.0\",\"features\":[{\"featureId\":\"rt_transfer_files\",\"attributes\":{\"serverSize\":\"6.08 GB\",\"serviceId\":\"jfrt@01g8dj3wcw22y01atqp63n1haq\"}}]}", 0},
+		{"jfrog-client-go", "rt_download", "", "3.58 GB", "{\"productId\":\"jfrog-client-go\",\"features\":[{\"featureId\":\"rt_download\"}]}", 1},
+		{"test", "rt_build", "jfrt@01g8dj3wcw22y01atqp63n1haq", "", "", 1},
+		{"agent/1.25.0", "rt_go", "", "", "", 1},
 	}
 
 	var tests []test
 	// Create the expected json
 	for _, test := range preTests {
-		test.expectedResult = fmt.Sprintf(json, test.productId, test.commandName)
+		if test.serverSize != "" && test.serviceId != "" {
+			test.expectedResult = fmt.Sprintf(jsonPatterns[test.jsonPatternNum], test.productId, test.commandName, test.serverSize, test.serviceId)
+		} else {
+			test.expectedResult = fmt.Sprintf(jsonPatterns[test.jsonPatternNum], test.productId, test.commandName)
+		}
+
 		tests = append(tests, test)
 	}
 
 	for _, test := range tests {
 		t.Run(test.commandName, func(t *testing.T) {
-			body, err := reportUsageToJson(test.productId, test.commandName)
-			if err != nil {
-				t.Error(err)
-			}
-
-			if string(body) != test.expectedResult {
-				t.Error(fmt.Errorf("expected %s, got %s", test.expectedResult, string(body)))
-			}
+			body, err := reportUsageToJson(test.productId, test.commandName, test.serviceId, test.serverSize)
+			assert.NoError(t, err)
+			assert.Equal(t, test.expectedResult, string(body))
 		})
 	}
 }
