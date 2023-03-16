9a740516d9d5d6cfc372df366fd3592a998117b0
Michael Sverdlov <michaelsv@jfrog.com>
2023-03-16 18:59:49 +0200

    Promoted version to 1.27.0

--- utils/utils.go
+++ utils/utils.go
@@ -26,7 +26,7 @@ import (
 const (
 	Development = "development"
 	Agent       = "jfrog-client-go"
-	Version     = "1.26.5"
+	Version     = "1.27.0"
 )
 
 // In order to limit the number of items loaded from a reader into the memory, we use a buffers with this size limit.
