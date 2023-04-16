a174173408b4194525034b6ed10627b9aba0d5ae
Michael Sverdlov <sverdlov93@gmail.com>
2023-04-16 16:56:19 +0300

    Add polling executor to Xray Watches create/update tests (#742)

--- pipelines/services/run.go
+++ pipelines/services/run.go
@@ -115,7 +115,7 @@ func (rs *RunService) TriggerPipelineRun(branch, pipeline string, isMultiBranch 
 }
 
 func (rs *RunService) CancelRun(runID int) error {
-	log.Info("Cancelling the run ", runID)
+	log.Info("Cancelling the run", runID)
 	runValue := strconv.Itoa(runID)
 	cancelRun := cancelRunPath
 	cancelRun = strings.Replace(cancelRun, ":runId", runValue, 1)
