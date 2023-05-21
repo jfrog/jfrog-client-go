ed65b88d24b6787a66fffe39367a45e7356265e7
Omer Zidkoni <omerz@jfrog.com>
2023-05-21 18:34:13 +0300

    Update build-info-go

--- utils/utils.go
+++ utils/utils.go
@@ -26,7 +26,7 @@ import (
 const (
 	Development = "development"
 	Agent       = "jfrog-client-go"
-	Version     = "1.28.5"
+	Version     = "1.28.6"
 )
 
 // In order to limit the number of items loaded from a reader into the memory, we use a buffers with this size limit.
