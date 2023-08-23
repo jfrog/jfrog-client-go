e47d7f8447e46b981674cf71ccb8408cade50fd4
delarea <eyalde@jfrog.com>
2023-08-23 16:46:34 +0300

    test xsc without context

--- tests/xsc_test.go
+++ tests/xsc_test.go
@@ -12,54 +12,45 @@ import (
 	"github.com/jfrog/jfrog-client-go/artifactory/services/utils/tests/xray"
 )
 
-func TestXscVersion(t *testing.T) {
-	initXscTest(t)
-	version := GetXrayDetails().GetXscVersion()
-	if version == "" {
-		t.Error("Expected a version, got empty string")
-	}
-}
-
 func TestXscScanGraph(t *testing.T) {
 	initXscTest(t)
-	mockScanId := "9c9dbd61-f544-4e33-4613-34727043d71f"
+	expectedScanId := "9c9dbd61-f544-4e33-4613-34727043d71f"
 	mockMultiScanId := "f2a8d4fe-40e6-11ee-84e4-02ee10c7f40e"
 
-	graphParams := &scan.XrayGraphScanParams{XscGitInfoContext: &scan.XscGitInfoContext{}}
-	scanId, err := securityServiceManager.ScanGraph(graphParams)
-	assert.NoError(t, err)
-	assert.Equal(t, mockMultiScanId, graphParams.MultiScanId)
-	assert.Equal(t, mockScanId, scanId)
+	tests := []struct {
+		name                string
+		xrayGraphParams     *scan.XrayGraphScanParams
+		expectedMultiScanId string
+	}{
+		{
+			name:                "XscScanWithContext",
+			xrayGraphParams:     &scan.XrayGraphScanParams{XscGitInfoContext: &scan.XscGitInfoContext{}},
+			expectedMultiScanId: mockMultiScanId,
+		}, {
+			name:                "XscScanNoContext",
+			xrayGraphParams:     &scan.XrayGraphScanParams{},
+			expectedMultiScanId: "",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			scanId, err := securityServiceManager.ScanGraph(test.xrayGraphParams)
+			assert.NoError(t, err)
+			assert.Equal(t, test.expectedMultiScanId, test.xrayGraphParams.MultiScanId)
+			assert.Equal(t, expectedScanId, scanId)
 
-	_, err = securityServiceManager.GetScanGraphResults(scanId, false, false)
-	assert.NoError(t, err)
-}
-
-func prepareXscTest(t *testing.T) {
-	xscDetails := mockXscServer()
-	// initialize securityServiceManager which can be accessed in the tests.
-	createSecurityManager(t, xscDetails)
+			_, err = securityServiceManager.GetScanGraphResults(scanId, false, false)
+			assert.NoError(t, err)
+		})
+	}
 }
 
 func TestXscEnabled(t *testing.T) {
 	initXscTest(t)
-
 	enabled, version, err := securityServiceManager.IsXscEnabled()
 	assert.NoError(t, err)
 	assert.Equal(t, true, enabled)
 	assert.Equal(t, "0.0.0", version)
-}
-
-func createSecurityManager(t *testing.T, xscDetails testXrayDetails) {
-	cfp := auth.ServiceDetails(xscDetails)
-	serviceConfig, err := config.NewConfigBuilder().
-		SetServiceDetails(cfp).
-		Build()
-	assert.NoError(t, err)
-	securityServiceManager, err = manager.New(serviceConfig)
-	assert.NoError(t, err)
-	// Assert correct security manager
-	assertSecurityManagerType(t)
 }
 
 func initXscTest(t *testing.T) {
@@ -69,12 +60,29 @@ func initXscTest(t *testing.T) {
 	prepareXscTest(t)
 }
 
-func mockXscServer() testXrayDetails {
+func prepareXscTest(t *testing.T) {
+	initializeTestSecurityManager(t, initMockXscServer())
+}
+
+func initMockXscServer() testXrayDetails {
 	xrayServerPort := xray.StartXrayMockServer()
 	xrayDetails := newTestXrayDetails(GetXrayDetails())
+	// Reroutes URLs to mock server
 	xrayDetails.SetUrl("http://localhost:" + strconv.Itoa(xrayServerPort) + "/xray/")
 	xrayDetails.SetXscUrl("http://localhost:" + strconv.Itoa(xrayServerPort) + "/xsc/")
 	return xrayDetails
+}
+
+func initializeTestSecurityManager(t *testing.T, xscDetails testXrayDetails) {
+	cfp := auth.ServiceDetails(xscDetails)
+	serviceConfig, err := config.NewConfigBuilder().
+		SetServiceDetails(cfp).
+		Build()
+	assert.NoError(t, err)
+	securityServiceManager, err = manager.New(serviceConfig)
+	assert.NoError(t, err)
+	// Assert correct security manager Xsc/Xray
+	assertSecurityManagerType(t)
 }
 
 func assertSecurityManagerType(t *testing.T) {
