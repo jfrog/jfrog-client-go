14887b84b87aacaf3ee37a80ffae2a87b746b47a
YardenEdery <72627596+YardenEdery@users.noreply.github.com>
2023-11-26 15:44:42 +0200

    Support for Distribution path mapping with AQL (#861)

--- utils/distribution/utils_test.go
+++ utils/distribution/utils_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestCreatePathMappings(t *testing.T) {
+func TestCreatePathMappingsFromPatternAndTarget(t *testing.T) {
 	tests := []struct {
 		specPattern           string
 		specTarget            string
@@ -27,7 +27,41 @@ func TestCreatePathMappings(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.specPattern, func(t *testing.T) {
 			specFile := &utils.CommonParams{Pattern: test.specPattern, Target: test.specTarget}
-			pathMappings := CreatePathMappings(specFile.Pattern, specFile.Target)
+			pathMappings := CreatePathMappingsFromPatternAndTarget(specFile.Pattern, specFile.Target)
+			if test.expectedMappingInput == "" {
+				assert.Empty(t, pathMappings)
+				return
+			}
+			assert.Len(t, pathMappings, 1)
+			actualPathMapping := pathMappings[0]
+			assert.Equal(t, test.expectedMappingInput, actualPathMapping.Input)
+			assert.Equal(t, test.expectedMappingOutput, actualPathMapping.Output)
+		})
+	}
+}
+
+func TestCreatePathMappings(t *testing.T) {
+	tests := []struct {
+		specInput             string
+		specOutput            string
+		expectedMappingInput  string
+		expectedMappingOutput string
+	}{
+		{"", "", "", ""},
+		{"repo/path/file.in", "", "", ""},
+		{"", "repo/path/file.out", "", ""},
+		{"a/b/c", "a/b/x", "a/b/c", "a/b/x"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.specInput, func(t *testing.T) {
+			specFile := &utils.CommonParams{
+				PathMapping: utils.PathMapping{
+					Input:  test.specInput,
+					Output: test.specOutput,
+				},
+			}
+			pathMappings := CreatePathMappings(specFile.PathMapping.Input, specFile.PathMapping.Output)
 			if test.expectedMappingInput == "" {
 				assert.Empty(t, pathMappings)
 				return
