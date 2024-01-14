514f283439ba63662806378049f48e08ff2ebaa2
Michael Sverdlov <michaelsv@jfrog.com>
2024-01-14 18:13:01 +0200

    fix conda
    Signed-off-by: Michael Sverdlov <michaelsv@jfrog.com>

--- tests/artifactoryupload_test.go
+++ tests/artifactoryupload_test.go
@@ -438,7 +438,7 @@ func createUploadFilesWithFailureHandlers() *testutils.HttpServerHandlers {
 	handlers := testutils.HttpServerHandlers{}
 	counter := 0
 	//nolint:unparam
-	handlers["/generic"] = func(w http.ResponseWriter, r *http.Request) {
+	handlers["generic"] = func(w http.ResponseWriter, r *http.Request) {
 		if counter == 0 {
 			fmt.Fprintln(w, "{\"checksums\":{\"sha256\":\"123\"}}")
 			w.WriteHeader(http.StatusOK)
