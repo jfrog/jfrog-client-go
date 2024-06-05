14b3c7c2f01f5f8893926618850c5183cd3eb3d0
Nathan Cooke <ncooke@shipt.com>
2024-06-05 09:55:50 -0500

    add support for XRay ignore_rules API

--- xray/services/report.go
+++ xray/services/report.go
@@ -3,11 +3,12 @@ package services
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+
 	"github.com/jfrog/jfrog-client-go/artifactory/services/utils"
 	"github.com/jfrog/jfrog-client-go/auth"
 	"github.com/jfrog/jfrog-client-go/http/jfroghttpclient"
 	"github.com/jfrog/jfrog-client-go/utils/errorutils"
-	"net/http"
 )
 
 const (
