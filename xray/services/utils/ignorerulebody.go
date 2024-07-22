d8a825148683de0c7bc95162dc27ddc34f68d59f
Nathan Cooke <ncooke@shipt.com>
2024-07-22 16:24:06 -0500

    cleanup and added functionality to get all by filter

--- xray/services/utils/ignorerulebody.go
+++ xray/services/utils/ignorerulebody.go
@@ -1,6 +1,9 @@
 package utils
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type IgnoreRuleParams struct {
 	Notes         string        `json:"notes"`
@@ -62,4 +65,102 @@ func CreateIgnoreRuleBody(ignoreRuleParams IgnoreRuleParams) IgnoreRuleBody {
 	return IgnoreRuleBody{
 		IgnoreRuleParams: ignoreRuleParams,
 	}
+}
+
+type IgnoreRulesGetAllParams struct {
+	Vulnerability        string    `json:"vulnerability"`
+	License              string    `json:"license"`
+	Policy               string    `json:"policy"`
+	Watch                string    `json:"watch"`
+	ComponentName        string    `json:"component_name"`
+	ComponentVersion     string    `json:"component_version"`
+	ArtifactName         string    `json:"artifact_name"`
+	ArtifactVersion      string    `json:"artifact_version"`
+	BuildName            string    `json:"build_name"`
+	BuildVersion         string    `json:"build_version"`
+	ReleaseBundleName    string    `json:"release_bundle_name"`
+	ReleaseBundleVersion string    `json:"release_bundle_version"`
+	DockerLayer          string    `json:"docker_layer"`
+	ExpiresBefore        time.Time `json:"expires_before"`
+	ExpiresAfter         time.Time `json:"expires_after"`
+	ProjectKey           string    `json:"project_key"`
+	OrderBy              string    `json:"order_by"`
+	Direction            string    `json:"direction"`
+	PageNum              int       `json:"page_num"`
+	NumOfRows            int       `json:"num_of_rows"`
+}
+
+// IgnoreRuleResponse struct representing the entire JSON
+type IgnoreRuleResponse struct {
+	Data       []IgnoreRuleBody `json:"data"`
+	TotalCount int              `json:"total_count"`
+}
+
+func (p *IgnoreRulesGetAllParams) GetParamMap() map[string]string {
+	params := make(map[string]string)
+	if p == nil {
+		return params
+	}
+	if p.Vulnerability != "" {
+		params["vulnerability"] = p.Vulnerability
+	}
+	if p.License != "" {
+		params["license"] = p.License
+	}
+	if p.Policy != "" {
+		params["policy"] = p.Policy
+	}
+	if p.Watch != "" {
+		params["watch"] = p.Watch
+	}
+	if p.ComponentName != "" {
+		params["component_name"] = p.ComponentName
+	}
+	if p.ComponentVersion != "" {
+		params["component_version"] = p.ComponentVersion
+	}
+	if p.ArtifactName != "" {
+		params["artifact_name"] = p.ArtifactName
+	}
+	if p.ArtifactVersion != "" {
+		params["artifact_version"] = p.ArtifactVersion
+	}
+	if p.BuildName != "" {
+		params["build_name"] = p.BuildName
+	}
+	if p.BuildVersion != "" {
+		params["build_version"] = p.BuildVersion
+	}
+	if p.ReleaseBundleName != "" {
+		params["release_bundle_name"] = p.ReleaseBundleName
+	}
+	if p.ReleaseBundleVersion != "" {
+		params["release_bundle_version"] = p.ReleaseBundleVersion
+	}
+	if p.DockerLayer != "" {
+		params["docker_layer"] = p.DockerLayer
+	}
+	if p.OrderBy != "" {
+		params["order_by"] = p.OrderBy
+	}
+	if p.Direction != "" {
+		params["direction"] = p.Direction
+	}
+	if p.PageNum != 0 {
+		params["page_num"] = fmt.Sprintf("%d", p.PageNum)
+	}
+	if p.NumOfRows != 0 {
+		params["num_of_rows"] = fmt.Sprintf("%d", p.NumOfRows)
+	}
+	if !p.ExpiresBefore.IsZero() {
+		params["expires_before"] = p.ExpiresBefore.UTC().Format(time.RFC3339)
+	}
+	if !p.ExpiresAfter.IsZero() {
+		params["expires_after"] = p.ExpiresAfter.UTC().Format(time.RFC3339)
+	}
+	if p.ProjectKey != "" {
+		params["project_key"] = p.ProjectKey
+	}
+
+	return params
 }
