c4b9ef90c453136bc84da680b326b193b7778ab8
eyalbe4 <eyalb@jfrog.com>
2024-12-20 22:02:17 +0200

    Internal API for the Visibility System

---
+++ jfconnect/services/metrics.go
@@ -0,0 +1,38 @@
+package services
+
+import (
+	"net/http"
+
+	rtUtils "github.com/jfrog/jfrog-client-go/artifactory/services/utils"
+	"github.com/jfrog/jfrog-client-go/auth"
+	"github.com/jfrog/jfrog-client-go/http/jfroghttpclient"
+	clientutils "github.com/jfrog/jfrog-client-go/utils"
+	"github.com/jfrog/jfrog-client-go/utils/errorutils"
+)
+
+type JfConnectService struct {
+	client         *jfroghttpclient.JfrogHttpClient
+	serviceDetails *auth.ServiceDetails
+}
+
+func NewJfConnectService(serviceDetails auth.ServiceDetails, client *jfroghttpclient.JfrogHttpClient) *JfConnectService {
+	return &JfConnectService{serviceDetails: &serviceDetails, client: client}
+}
+
+func (js *JfConnectService) GetJfConnectDetails() auth.ServiceDetails {
+	return *js.serviceDetails
+}
+
+func (js *JfConnectService) LogMetric(metric []byte) error {
+	details := js.GetJfConnectDetails()
+	httpClientDetails := details.CreateHttpClientDetails()
+	rtUtils.SetContentType("application/json", &httpClientDetails.Headers)
+
+	url := clientutils.AddTrailingSlashIfNeeded(details.GetUrl())
+	url = url + "jfconnect/api/v1/backoffice/metrics/log"
+	resp, body, err := js.client.SendPost(url, metric, &httpClientDetails)
+	if err != nil {
+		return err
+	}
+	return errorutils.CheckResponseStatusWithBody(resp, body, http.StatusCreated, http.StatusOK)
+}
