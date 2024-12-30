e342ed5065f156d265c31bf7bfe2eaddc0465eea
Assaf Attias <49212512+attiasas@users.noreply.github.com>
2024-12-30 17:46:16 +0200

    Add Git Repository Resource in Xray API's (#1039)

--- xray/services/scan_test.go
+++ xray/services/scan_test.go
@@ -8,39 +8,48 @@ import (
 func TestCreateScanGraphQueryParams(t *testing.T) {
 	tests := []struct {
 		testName      string
-		projectKey    string
-		repoPath      string
-		watches       []string
-		scanType      ScanType
+		params        XrayGraphScanParams
 		expectedQuery string
 	}{
-		{"with_project_key", "p1", "", nil, Binary,
-			fmt.Sprintf("?%s%s&%s%s", projectQueryParam, "p1", scanTypeQueryParam, Binary)},
-
-		{"with_repo_path", "", "r1", nil, Binary,
-			fmt.Sprintf("?%s%s&%s%s", repoPathQueryParam, "r1", scanTypeQueryParam, Binary)},
-
-		{"with_watches", "", "", []string{"w1", "w2"}, Binary,
-			fmt.Sprintf("?%s%s&%s%s&%s%s", watchesQueryParam, "w1", watchesQueryParam, "w2", scanTypeQueryParam, Binary)},
-
-		{"with_empty_watch_string", "", "", []string{""}, "",
-			""},
-
-		{"without_context", "", "", nil, Dependency,
-			fmt.Sprintf("?%s%s", scanTypeQueryParam, Dependency)},
-
-		{"without_scan_type", "", "", []string{"w1", "w2"}, "",
-			fmt.Sprintf("?%s%s&%s%s", watchesQueryParam, "w1", watchesQueryParam, "w2")},
+		{
+			testName:      "with_project_key",
+			params:        XrayGraphScanParams{ProjectKey: "p1", ScanType: Binary},
+			expectedQuery: fmt.Sprintf("?%s%s&%s%s", projectQueryParam, "p1", scanTypeQueryParam, Binary),
+		},
+		{
+			testName:      "with_repo_path",
+			params:        XrayGraphScanParams{RepoPath: "r1", ScanType: Binary},
+			expectedQuery: fmt.Sprintf("?%s%s&%s%s", repoPathQueryParam, "r1", scanTypeQueryParam, Binary),
+		},
+		{
+			testName:      "with_watches",
+			params:        XrayGraphScanParams{Watches: []string{"w1", "w2"}, ScanType: Binary},
+			expectedQuery: fmt.Sprintf("?%s%s&%s%s&%s%s", watchesQueryParam, "w1", watchesQueryParam, "w2", scanTypeQueryParam, Binary),
+		},
+		{
+			testName:      "with_empty_watch_string",
+			params:        XrayGraphScanParams{Watches: []string{""}},
+			expectedQuery: "",
+		},
+		{
+			testName:      "without_context",
+			params:        XrayGraphScanParams{ScanType: Dependency, XrayVersion: MinXrayVersionGitRepoKey},
+			expectedQuery: fmt.Sprintf("?%s%s", scanTypeQueryParam, Dependency),
+		},
+		{
+			testName:      "without_scan_type",
+			params:        XrayGraphScanParams{Watches: []string{"w1", "w2"}},
+			expectedQuery: fmt.Sprintf("?%s%s&%s%s", watchesQueryParam, "w1", watchesQueryParam, "w2"),
+		},
+		{
+			testName:      "with_git_repo_url",
+			params:        XrayGraphScanParams{GitRepoHttpsCloneUrl: "http://some-url", ScanType: Dependency, XrayVersion: MinXrayVersionGitRepoKey},
+			expectedQuery: fmt.Sprintf("?%s%s&%s%s", scanTypeQueryParam, Dependency, gitRepoKeyQueryParam, "some-url.git"),
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.testName, func(t *testing.T) {
-			params := XrayGraphScanParams{
-				RepoPath:   test.repoPath,
-				Watches:    test.watches,
-				ProjectKey: test.projectKey,
-				ScanType:   test.scanType,
-			}
-			actualQuery := createScanGraphQueryParams(params)
+			actualQuery := createScanGraphQueryParams(test.params)
 			if actualQuery != test.expectedQuery {
 				t.Error(test.testName, "Expecting:", test.expectedQuery, "Got:", actualQuery)
 			}
