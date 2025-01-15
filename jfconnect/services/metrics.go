d0737923e44a522f04f8ca2a870d819a1a1bb678
Omer Zidkoni <50792403+omerzi@users.noreply.github.com>
2025-01-15 16:32:37 +0200

    Restructure visibility metrics

--- jfconnect/services/metrics.go
+++ jfconnect/services/metrics.go
@@ -1,19 +1,49 @@
 package services
 
-type VisibilityMetric interface {
-	MetricValue() int
-	MetricName() string
+import (
+	"encoding/json"
+	"github.com/jfrog/jfrog-client-go/auth"
+	"github.com/jfrog/jfrog-client-go/http/jfroghttpclient"
+	clientutils "github.com/jfrog/jfrog-client-go/utils"
+	"github.com/jfrog/jfrog-client-go/utils/errorutils"
+	"net/http"
+)
+
+const LogMetricApiEndpoint = "api/v1/backoffice/metrics/log"
+
+type VisibilityMetric struct {
+	Value  int    `json:"value"`
+	Name   string `json:"metrics_name"`
+	Labels any    `json:"labels"`
 }
 
-type Metric struct {
-	Value int    `json:"value"`
-	Name  string `json:"metrics_name"`
+type JfConnectService struct {
+	client         *jfroghttpclient.JfrogHttpClient
+	serviceDetails *auth.ServiceDetails
 }
 
-func (m *Metric) MetricValue() int {
-	return m.Value
+func NewJfConnectService(serviceDetails auth.ServiceDetails, client *jfroghttpclient.JfrogHttpClient) *JfConnectService {
+	return &JfConnectService{serviceDetails: &serviceDetails, client: client}
 }
 
-func (m *Metric) MetricName() string {
-	return m.Name
+func (jcs *JfConnectService) GetJfConnectDetails() auth.ServiceDetails {
+	return *jcs.serviceDetails
+}
+
+func (jcs *JfConnectService) PostVisibilityMetric(metric VisibilityMetric) error {
+	metricJson, err := json.Marshal(metric)
+	if err != nil {
+		return errorutils.CheckError(err)
+	}
+	details := jcs.GetJfConnectDetails()
+	httpClientDetails := details.CreateHttpClientDetails()
+	httpClientDetails.SetContentTypeApplicationJson()
+
+	url := clientutils.AddTrailingSlashIfNeeded(details.GetUrl())
+	url += LogMetricApiEndpoint
+	resp, body, err := jcs.client.SendPost(url, metricJson, &httpClientDetails)
+	if err != nil {
+		return err
+	}
+	return errorutils.CheckResponseStatusWithBody(resp, body, http.StatusCreated, http.StatusOK)
 }
