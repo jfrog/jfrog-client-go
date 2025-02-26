166303f71e6d3443caf4ece08a5c6d1c872a89db
Eran Turgeman <erant@jfrog.com>
2025-02-26 18:10:59 +0200

    adding const and comment

--- xsc/services/profile.go
+++ xsc/services/profile.go
@@ -14,8 +14,10 @@ import (
 )
 
 const (
+	// TODO eran update all CC min versions in frogbot and security
 	ConfigProfileMinXscVersion                = "1.11.0"
 	ConfigProfileByUrlMinXrayVersion          = "3.110.0"
+	ConfigProfileNewSchemaMinXrayVersion      = "3.116.0"
 	xscConfigProfileByNameApi                 = "profile"
 	xscConfigProfileByUrlApi                  = "profile_repos"
 	xscDeprecatedConfigProfileByNameApiSuffix = "api/v1/" + xscConfigProfileByNameApi
