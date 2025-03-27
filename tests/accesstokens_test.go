b157c650f0785570a878dc9e6b784b5ed79faa38
delarea <eyalde@jfrog.com>
2025-03-27 11:36:47 +0200

    add tests

--- tests/accesstokens_test.go
+++ tests/accesstokens_test.go
@@ -1,10 +1,16 @@
 package tests
 
 import (
+	"encoding/json"
+	accessAuth "github.com/jfrog/jfrog-client-go/access/auth"
 	"github.com/jfrog/jfrog-client-go/access/services"
 	"github.com/jfrog/jfrog-client-go/auth"
+	"github.com/jfrog/jfrog-client-go/http/jfroghttpclient"
 	"github.com/jfrog/jfrog-client-go/utils"
 	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -15,18 +21,79 @@ func TestAccessTokens(t *testing.T) {
 	t.Run("createAccessToken", testCreateRefreshableToken)
 	t.Run("createAccessTokenWithReference", testAccessTokenWithReference)
 	t.Run("refreshToken", testRefreshTokenTest)
-	t.Run("oidcTokenExchange", testExchangeOidcToken)
+	t.Run("exchangeOIDCToken", testExchangeOidcToken)
 }
 
+// Mocks exchange response from the server as it requires setting up a full OIDC flow
+// which currently is not supported by CLI commands.
 func testExchangeOidcToken(t *testing.T) {
 	initAccessTest(t)
-	// Setup
+
+	// Create mock server
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+		assert.Equal(t, "/access/api/v1/oidc/token", r.URL.Path)
+
+		// Verify request body
+		body, err := io.ReadAll(r.Body)
+		assert.NoError(t, err)
+		defer func() {
+			assert.NoError(t, r.Body.Close())
+		}()
+
+		var req services.CreateOidcTokenParams
+		err = json.Unmarshal(body, &req)
+		assert.NoError(t, err)
+		assert.Equal(t, "mockOidcTokenID", req.OidcTokenID)
+
+		// Simulate response
+		resp := auth.OidcTokenResponseData{
+			CommonTokenParams: auth.CommonTokenParams{
+				AccessToken: "mockAccessToken",
+			},
+			IssuedTokenType: "mockIssuedTokenType",
+			Username:        "mockUsername",
+		}
+		responseBody, err := json.Marshal(resp)
+		assert.NoError(t, err)
+
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(responseBody)
+		assert.NoError(t, err)
+	})
+	ts := httptest.NewServer(handler)
+	defer ts.Close()
+
+	// Setup JFrog client
+	client, err := jfroghttpclient.JfrogClientBuilder().
+		SetInsecureTls(true).
+		Build()
+	assert.NoError(t, err, "Failed to create JFrog client")
+
+	// Setup TokenService
+	service := services.NewTokenService(client)
+	serverDetails := accessAuth.NewAccessDetails()
+	serverDetails.SetUrl(ts.URL + "/access")
+	service.ServiceDetails = serverDetails
+
+	// Define OIDC token parameters
 	params := services.CreateOidcTokenParams{
-		GrantType: "authorization_code",
+		GrantType:        "authorization_code",
+		SubjectTokenType: "Generic",
+		OidcTokenID:      "mockOidcTokenID",
+		ProviderName:     "mockProviderName",
+		ProjectKey:       "mockProjectKey",
+		JobId:            "mockJobId",
+		RunId:            "mockRunId",
+		Repo:             "mockRepo",
+		ApplicationKey:   "mockApplicationKey",
+		Audience:         "mockAudience",
 	}
-	// Execute
-	response, err := testsAccessTokensService.ExchangeOidcToken(params)
-	// Verify
+
+	// Execute ExchangeOidcToken
+	response, err := service.ExchangeOidcToken(params)
+
+	// Verify response
 	assert.NoError(t, err)
 	assert.NotNil(t, response)
 }
