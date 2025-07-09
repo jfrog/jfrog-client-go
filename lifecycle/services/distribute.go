5cfe81b060954cdee2e13ae10b35800da9342c2e
Naveen Kumar <naveenku@jfrog.com>
2025-07-09 17:37:11 +0530

    RTECO-235 - Fixed the proper handling of deletionStatus (#1142)

--- lifecycle/services/distribute.go
+++ lifecycle/services/distribute.go
@@ -14,6 +14,7 @@ import (
 )
 
 const (
+	releaseBundleNewApi = "api/v2/release_bundle"
 	distributionBaseApi = "api/v2/distribution/"
 	distribute          = "distribute"
 	trackers            = "trackers"
