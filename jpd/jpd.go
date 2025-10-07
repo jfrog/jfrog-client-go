d9e78a2703e2026c9c7da6990f353220f679cff4
Naveen Kumar <naveenku@jfrog.com>
2025-10-07 20:45:33 +0530

    Updating Display field value

--- jpd/jpd.go
+++ jpd/jpd.go
@@ -20,8 +20,8 @@ type JPDsStatsService struct {
 }
 
 type GenericError struct {
-	Product string `display:"-"`
-	Err     error  `display:"Error"`
+	Product string
+	Err     error `display:"Error"`
 }
 
 func NewJPDsStatsService(artDetails auth.ServiceDetails, client *jfroghttpclient.JfrogHttpClient) *JPDsStatsService {
