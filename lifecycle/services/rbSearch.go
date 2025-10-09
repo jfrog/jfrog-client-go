11aaf63624a99a618a0cb784ea9fcbd9b0da403d
Naveen Kumar <naveenku@jfrog.com>
2025-10-09 22:46:09 +0530

    Temp commenting const

--- lifecycle/services/rbSearch.go
+++ lifecycle/services/rbSearch.go
@@ -4,15 +4,6 @@ import (
 	"github.com/jfrog/jfrog-client-go/utils/log"
 )
 
-//const (
-//	releaseBundleGroupAPI        = "lifecycle/api/v2/release_bundle/groups"
-//	releaseBundleVersionsAPI     = "lifecycle/api/v2/release_bundle/versions"
-//	releaseBundleArtifactsAPI    = "lifecycle/api/v2/release_bundle/artifacts"
-//	releaseBundleEnvironmentsAPI = "lifecycle/api/v2/release_bundle/environments"
-//	releaseBundleStatusAPI       = "lifecycle/api/v2/release_bundle/status"
-//	releaseBundleSignatureAPI    = "lifecycle/api/v2/release_bundle/signature"
-//)
-
 func (rbs *ReleaseBundlesService) RbSearchNames() error {
 	log.Debug("Not implemented yet")
 	return nil
